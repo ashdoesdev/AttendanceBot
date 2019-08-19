5afb019d8407abe61a5d7ec5813d3ba93c6151af
Ash <ashdoes.dev@gmail.com>
2019-08-18 21:51:22 -0700

    Use tables

--- dist/src/Embeds/minimal-visualization.embed.js
+++ dist/src/Embeds/minimal-visualization.embed.js
@@ -8,12 +8,26 @@ class MinimalVisualizationEmbed extends discord_js_1.RichEmbed {
         this.lootScoreMap = lootScoreMap;
         this._timestampHelper = new timestamp_helper_1.TimestampHelper();
         let memberLines = '';
+        let topSeparator = '╔═════════════════╦══════╦══════╦══════╦══════╦═══════════╗\n';
+        let separator = '╠═════════════════╬══════╬══════╬══════╬══════╬═══════════╣\n';
+        let bottomSeparator = '╚═════════════════╩══════╩══════╩══════╩══════╩═══════════╝\n';
+        let header = '║ Name            ║ Att. ║ Sen. ║ Main ║ O/S  ║ Last Loot ║\n';
         for (let member of lootScoreMap) {
-            memberLines += `**${member[0].displayName}**: ${member[1].attendancePercentage}% - ${member[1].seniorityPercentage}% - ${member[1].itemScoreTotal} -  ${member[1].itemScoreOffspecTotal} -  ${member[1].lastLootDate}\n`;
+            memberLines += separator;
+            let name = member[0].displayName.slice(0, 15).padEnd(15, ' ');
+            let attendance = `${member[1].attendancePercentage}%`.padEnd(4, ' ');
+            let seniority = `${member[1].seniorityPercentage}%`.padEnd(4, ' ');
+            let main = member[1].itemScoreTotal.toString().slice(0, 4).padEnd(4, ' ');
+            let offspec = member[1].itemScoreOffspecTotal.toString().slice(0, 4).padEnd(4, ' ');
+            let lastLootDate = member[1].lastLootDate.padEnd(9, ' ');
+            memberLines += `║ ${name} ║ ${attendance} ║ ${seniority} ║ ${main} ║ ${offspec} ║ ${lastLootDate} ║\n`;
         }
         this.setColor('#60b5bc');
         this.setTitle(title);
-        this.addField('**Name**: Attendance - Seniority - ItemScore - Offspec - Last Loot Date', memberLines);
+        this.setDescription(this.codeBlockify(topSeparator + header + memberLines + bottomSeparator));
+    }
+    codeBlockify(string) {
+        return '```' + string + '```';
     }
 }
 exports.MinimalVisualizationEmbed = MinimalVisualizationEmbed;
--- src/Embeds/minimal-visualization.embed.ts
+++ src/Embeds/minimal-visualization.embed.ts
@@ -1,4 +1,4 @@
-import { RichEmbed, GuildMember } from "discord.js";
+﻿import { RichEmbed, GuildMember } from "discord.js";
 import { TimestampHelper } from "../Helpers/timestamp.helper";
 import { MemberScore } from "../Models/loot-score.model";
 
@@ -9,13 +9,31 @@ export class MinimalVisualizationEmbed extends RichEmbed {
         super();
 
         let memberLines: string = '';
+        let topSeparator = '╔═════════════════╦══════╦══════╦══════╦══════╦═══════════╗\n';
+        let separator = '╠═════════════════╬══════╬══════╬══════╬══════╬═══════════╣\n';
+        let bottomSeparator = '╚═════════════════╩══════╩══════╩══════╩══════╩═══════════╝\n';
+        let header = '║ Name            ║ Att. ║ Sen. ║ Main ║ O/S  ║ Last Loot ║\n';
 
         for (let member of lootScoreMap) {
-            memberLines += `**${member[0].displayName}**: ${member[1].attendancePercentage}% - ${member[1].seniorityPercentage}% - ${member[1].itemScoreTotal} -  ${member[1].itemScoreOffspecTotal} -  ${member[1].lastLootDate}\n`;
+            memberLines += separator;
+
+            let name = member[0].displayName.slice(0, 15).padEnd(15, ' ');
+            let attendance = `${member[1].attendancePercentage}%`.padEnd(4, ' ');
+            let seniority = `${member[1].seniorityPercentage}%`.padEnd(4, ' ');
+            let main = member[1].itemScoreTotal.toString().slice(0, 4).padEnd(4, ' ');
+            let offspec = member[1].itemScoreOffspecTotal.toString().slice(0, 4).padEnd(4, ' ');
+            let lastLootDate = member[1].lastLootDate.padEnd(9, ' ');
+
+            memberLines += `║ ${name} ║ ${attendance} ║ ${seniority} ║ ${main} ║ ${offspec} ║ ${lastLootDate} ║\n`;
         }
 
         this.setColor('#60b5bc');
         this.setTitle(title);
-        this.addField('**Name**: Attendance - Seniority - ItemScore - Offspec - Last Loot Date', memberLines);
+        this.setDescription(this.codeBlockify(topSeparator + header + memberLines + bottomSeparator));
+    }
+
+    private codeBlockify(string: string): string {
+        return '```' + string + '```';
     }
 }
+
