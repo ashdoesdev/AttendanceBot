ec92e3a5206fda5bc2b7f50a4439805779667b04
Ash <ashdoes.dev@gmail.com>
2021-03-27 16:52:09 -0700

    Clean up old files

--- dist/src/Helpers/timestamp.helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TimestampHelper {
-    get monthDayYearFormatted() {
-        return `${new Date().getMonth() + 1}-${new Date().getDate()}-${new Date().getFullYear()}`;
-    }
-}
-exports.TimestampHelper = TimestampHelper;
--- dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const attendance_bot_1 = require("./attendance-bot");
-const appSettings = require("../appSettings.dev.json");
-const attendanceBot = new attendance_bot_1.AttendanceBot();
-attendanceBot.start(appSettings);
